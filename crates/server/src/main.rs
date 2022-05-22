213d4f918cf8e43b98024e02c3739fb4467cee07
Fábio Luciano <fabio@naoimporta.com>
2022-05-21 21:02:14 -0300

    feature: init command

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,15 +1,19 @@
 #[macro_use]
 extern crate rocket;
 
+use common::database::get_connection;
+use entity::url::Entity as Url;
+use rocket::serde::json::Json;
+use sea_orm::EntityTrait;
+use sea_orm::JsonValue;
+
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+async fn hello() -> Json<Vec<JsonValue>> {
+    let connection = get_connection().await;
+    Json(Url::find().into_json().all(&connection).await.unwrap())
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![hello])
-        .mount("/hi", routes![hello])
-        .mount("/hello", routes![hello])
+    rocket::build().mount("/", routes![hello])
 }
