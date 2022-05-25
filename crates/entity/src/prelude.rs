12e9e5046e1824e23cae4eee402feef9edf92836
Fábio Luciano <fabio@naoimporta.com>
2022-05-25 20:49:37 -0300

    feat: add adhoc methods

--- crates/entity/src/prelude.rs
+++ crates/entity/src/prelude.rs
@@ -1,7 +1,10 @@
 pub use super::seaql_migrations::Entity as SeaqlMigrations;
+
 pub use super::tag::ActiveModel as TagActiveModel;
 pub use super::tag::Entity as Tag;
+
 pub use super::url::ActiveModel as UrlActiveModel;
 pub use super::url::Entity as Url;
+
 pub use super::url_tag::ActiveModel as UrlTagActiveModel;
 pub use super::url_tag::Entity as UrlTag;
